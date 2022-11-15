52eabd9557c1361e0667a0a37a07d0803c65baaa
Ilya Makarov <mortimer08@gmail.com>
2022-11-15 22:50:43 +0500

    Task41 Complete

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// -1, -7, 567, 89, 223-> 3
+
+int[] InputArray(int numM)
+{
+    int[] arr = new int[numM];
+    for (int i = 0; i < numM; i++)
+    {
+        Console.Write($"Ведите число номер {i + 1}: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+int PositiveNumbersCount(int[]arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i]>0)count++;
+    }
+    return count;
+
+}
+
+Console.Write("Введите количество чисел: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+int[] array = InputArray(numberM);
+Console.WriteLine($"Введено чисел больше нуля: {PositiveNumbersCount(array)}");
